28418a07615442c364a324d49ee266210a9510a9
Your Name <you@example.com>
2026-01-01 17:11:12 +0900

    行列計算ツールを開始

---
+++ build/CMakeFiles/matrix_tool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_tool.dir/src/main.c.o"
+  "CMakeFiles/matrix_tool.dir/src/main.c.o.d"
+  "matrix_tool"
+  "matrix_tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matrix_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+if (argc > 0) {
+        printf("Running: %s\n", argv[0]);
+    }
+    printf("Hello world!\n");
+    return 0;
+}
